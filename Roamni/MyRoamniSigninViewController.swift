70e6a261bc72575d7df557b3be32917927f5821b
Michael0770 <hyman0770@gmail.com>
2017-09-20 23:40:06 +1000

    YES

--- Roamni/MyRoamniSigninViewController.swift
+++ Roamni/MyRoamniSigninViewController.swift
@@ -8,14 +8,59 @@
 
 import UIKit
 
-class MyRoamniSigninViewController: UIViewController {
+class MyRoamniSigninViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
     
+    var countries: [String] = []
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var repassword: UITextField!
+    @IBOutlet weak var firstnameField: UITextField!
+    
+    @IBOutlet weak var lastnameField: UITextField!
+    
+    @IBOutlet weak var aboutmeText: UITextView!
+    @IBOutlet weak var countryPicker: UIPickerView!
+    @IBOutlet weak var doneBtn: UIBarButtonItem!
     @IBOutlet weak var cancelBtn: UIBarButtonItem!
 
     override func viewDidLoad() {
         navigationController?.navigationBar.barTintColor = UIColor(red: 5.0/255.0, green: 24.0/255.0, blue: 57.0/255.0, alpha: 1.0)
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         cancelBtn.tintColor = UIColor.white
+        doneBtn.tintColor = UIColor.white
+        countryPicker.delegate = self
+        countryPicker.dataSource = self
+        aboutmeText.layer.borderWidth = 1.0
+        //aboutmeText.layer.bor = UIColor(red: 5.0/255.0, green: 24.0/255.0, blue: 57.0/255.0, alpha: 1.0) as! CGColor
+
+        
+        
+        
+        for code in NSLocale.isoCountryCodes as [String] {
+            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
+            let name = NSLocale(localeIdentifier: "en_UK").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
+            countries.append(name)
+        }
+        
+        print(countries)
+        //countryPicker.
+
 
         // Do any additional setup after loading the view.
     }
@@ -24,6 +69,20 @@ class MyRoamniSigninViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func done(_ sender: Any) {
+        if self.emailField.text == "" || self.passwordField.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
