e66809e093bd67178db25182b15ebd1c463999f4
Michael0770 <hyman0770@gmail.com>
2017-09-28 11:33:58 +1000

    yes

---
+++ Roamni/MyRoamniPaymentEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyRoamniPaymentEditViewController.swift
+//  Roamni
+//
+//  Created by Hyman Li on 28/9/17.
+//  Copyright © 2017 ROAMNI. All rights reserved.
+//
+
+import UIKit
+
+class MyRoamniPaymentEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
