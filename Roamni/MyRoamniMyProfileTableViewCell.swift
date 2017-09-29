9778268d3504ff146d3e95920efe1152dbb5bf21
Michael0770 <hyman0770@gmail.com>
2017-09-29 21:31:18 +1000

    profile

---
+++ Roamni/MyRoamniMyProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyRoamniMyProfileTableViewCell.swift
+//  Roamni
+//
+//  Created by Hyman Li on 29/9/17.
+//  Copyright © 2017 ROAMNI. All rights reserved.
+//
+
+import UIKit
+
+class MyRoamniMyProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Roamni/MyRoamniMyProfileViewController.swift
+++ Roamni/MyRoamniMyProfileViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
+import AVFoundation
 import FirebaseAuth
 import FBSDKLoginKit
-class MyRoamniMyProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class MyRoamniMyProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITableViewDataSource{
 
-    @IBOutlet weak var userImage: UIImageView!
+
+    
     @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var tableview: UITableView!
+    
+    @IBOutlet weak var aboutme: UITextView!
+    @IBOutlet weak var uploadTourNumber: UILabel!
+    
+    
     var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //self.tableView.rowHeight = 40.0
         // Do any additional setup after loading the view.
     }
 
@@ -27,16 +37,8 @@ class MyRoamniMyProfileViewController: UIViewController, UINavigationControllerD
         // Dispose of any resources that can be recreated.
     }
     
+
     @IBAction func changePhoto(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
-            print("Button capture")
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .savedPhotosAlbum;
-            imagePicker.allowsEditing = false
-            
-            self.present(imagePicker, animated: true, completion: nil)
-        }
     }
     
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
@@ -46,6 +48,26 @@ class MyRoamniMyProfileViewController: UIViewController, UINavigationControllerD
         
         userImage.image = image
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniMyProfileTableViewCell", for: indexPath)
+            as! MyRoamniMyProfileTableViewCell
+        //cell.frame.height = 40
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        print("high40")
+        return 40.0;//Choose your custom row height
+    }
+    
     /*
     // MARK: - Navigation
 
