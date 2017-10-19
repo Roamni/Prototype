8011b8a1f26492222fefc356d6c77090a5a577b0
Michael0770 <hyman0770@gmail.com>
2017-10-20 01:49:47 +1100

    yes

---
+++ Roamni/MyRoamniReviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyRoamniReviewTableViewCell.swift
+//  Roamni
+//
+//  Created by Hyman Li on 20/10/17.
+//  Copyright © 2017 ROAMNI. All rights reserved.
+//
+
+import UIKit
+
+class MyRoamniReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Roamni/ReviewsTableViewController.swift
+++ Roamni/ReviewsTableViewController.swift
@@ -35,12 +35,19 @@ class ReviewsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyRoamniReviewTableViewCell", for: indexPath) as! MyRoamniReviewTableViewCell
+        return cell
     }
 
     /*
